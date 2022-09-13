2a8e1c581a3db3b3701321b38fbfaf1a9fa3a972
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-13 11:00:06 +0700

    Update moodle_tests.py

--- moodle_tests.py
+++ moodle_tests.py
@@ -1,22 +1,11 @@
 import unittest
-import moodle_methods as methods
-import moodle_locators as locators
+import rmit_store_methods as methods
 
 
 class MoodleAppPositiveTestCases(unittest.TestCase):
 
     @staticmethod
     def test_database_connection():
-#         methods.log_in(locators.moodle_username, locators.moodle_password)
-#         methods.create_new_user()
-#         methods.check_user_created()
-#         methods.log_out()
-#         methods.log_in(locators.new_username, locators.new_password)
-#         methods.check_we_logged_in_with_new_cred()
-#         methods.log_out()
-#         methods.log_in(locators.moodle_username, locators.moodle_password)
-#         methods.delete_test_user()
-#         methods.log_out()
         methods.setUp()
         methods.checkDatabaseErrorAppear()
         methods.tearDown()
