47fe3dbd5eac13de492b5a0189d0644ada1450fa
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-13 11:14:15 +0700

    Update rmit_store_locators.py

--- rmit_store_locators.py
+++ rmit_store_locators.py
@@ -2,4 +2,4 @@ from faker import Faker
 
 fake = Faker(locale='en_CA')
 rmit_store_dev_url = 'http://18.208.182.182/'
-rmit_store_prod_url = 'http://18.208.182.182/'
+rmit_store_prod_url = 'http://18.208.888.888/'
