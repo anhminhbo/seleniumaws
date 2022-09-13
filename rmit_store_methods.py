ec0518cac3fc5d9b9e893e77b1797f7040a6463e
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-13 11:00:57 +0700

    Update and rename moodle_methods.py to rmit_store_methods.py

--- rmit_store_methods.py
+++ rmit_store_methods.py
@@ -1,6 +1,6 @@
 import sys
 import datetime
-import moodle_locators as locators
+import rmit_store_locators as locators
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
