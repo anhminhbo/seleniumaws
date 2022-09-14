d9546b47c19bf1ef09a0e4ddc163d161f4f19438
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-14 23:43:34 +0700

    Update rmit_store_locators.py

--- rmit_store_locators.py
+++ rmit_store_locators.py
@@ -1,5 +1,5 @@
 from faker import Faker
 
 fake = Faker(locale='en_CA')
-rmit_store_dev_url = 'http://18.208.182.182/'
-rmit_store_prod_url = 'http://18.208.888.888/'
+rmit_store_dev_url = 'http://50.19.121.44:8080/'
+rmit_store_prod_url = 'http://docker-1135570227.us-east-1.elb.amazonaws.com:8080/'
